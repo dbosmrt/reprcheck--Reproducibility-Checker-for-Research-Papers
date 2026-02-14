11085268f917034fe807efb4047364cdf711e3e5
dbosmrt <deepanshu.2006bhatt@gmail.com>
2026-02-14 16:06:27 +0530

    made some changes

--- parsing/pdf_markdown.py
+++
@@ -1,5 +0,0 @@
-"""
-Here I will be writing the script for converting all the pdf files to markdown.
-"""
-import os
-import logging
---
+++ server/parsing/pdf_to_markdown.py
@@ -0,0 +1,13 @@
+"""
+Here I will convert all the pdfs into markdown using marker-pdf library by Datalab.to
+"""
+import os
+import logging
+import marker
+from marker import markdown
+
+#configuration (Logging)
+logging.basicConfig(
+    level= INFO,
+    format=  ()
+)
