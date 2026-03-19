68b0d18cfa082b6b986e08900cdfeca9292d0120
dbosmrt <deepanshu.2006bhatt@gmail.com>
2026-03-19 16:55:41 +0530

    made some changes

---
+++ server/data_/data_cleaning.py
@@ -0,0 +1,14 @@
+"""
+This script contians all the methods which will be applied in order to clean the MD files.
+"""
+
+import os
+import logging
+
+
+#configuring logging
+logger = logging.basicConfig(
+    level= logging.INFO,
+    format= '%(astime)s - %(name)s - %(levelname)s - %(message)s '
+    datefmt='%Y -%m -%d %H:%M:%S'
+)
