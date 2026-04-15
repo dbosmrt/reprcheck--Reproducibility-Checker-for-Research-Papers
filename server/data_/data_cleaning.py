66baf9724ffa3b536dfe48492de6b5118d20b659
dbosmrt <deepanshu.2006bhatt@gmail.com>
2026-04-15 15:10:24 +0530

    Update data_cleaning.py

--- server/data_/data_cleaning.py
+++ server/data_/data_cleaning.py
@@ -9,6 +9,6 @@ import logging
 #configuring logging
 logger = logging.basicConfig(
     level= logging.INFO,
-    format= '%(astime)s - %(name)s - %(levelname)s - %(message)s '
+    format= '%(astime)s - %(name)s - %(levelname)s - %(message)s ',
     datefmt='%Y -%m -%d %H:%M:%S'
 )
